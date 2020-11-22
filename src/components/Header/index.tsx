cd0b0c6eb74557d78c04f04607850e6e978a5b11
Volodya Taras <proger.sdk@gmail.com>
2020-11-22 14:39:58 +0200

    Update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,16 +4,18 @@ import {
   Container,
   IconButton,
   Toolbar,
+  Tooltip,
   Typography,
 } from '@material-ui/core'
 import { ExitToApp } from '@material-ui/icons'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { signOutUser } from '../../redux/auth/actions'
 
 import styles from './style.module.scss'
 
 const Header = () => {
   const dispatch = useDispatch()
+  const username = useSelector((state) => state.auth.username)
 
   const onSignOutClick = () => {
     dispatch(signOutUser())
@@ -25,14 +27,21 @@ const Header = () => {
       <AppBar position="fixed">
         <Container maxWidth="xl" disableGutters>
           <Toolbar className={styles.toolbar}>
-            <Typography variant="h6">BLE Scanner</Typography>
-            <IconButton
-              color="inherit"
-              aria-label="exit"
-              onClick={onSignOutClick}
-            >
-              <ExitToApp />
-            </IconButton>
+            <div>
+              <Typography variant="h6">BLE Scanner</Typography>
+            </div>
+            <div className={styles.userPart}>
+              <Typography>Hello, {username}!</Typography>
+              <Tooltip title="Exit" aria-label="exit">
+                <IconButton
+                  color="inherit"
+                  aria-label="exit"
+                  onClick={onSignOutClick}
+                >
+                  <ExitToApp />
+                </IconButton>
+              </Tooltip>
+            </div>
           </Toolbar>
         </Container>
       </AppBar>
